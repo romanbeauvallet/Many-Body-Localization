113438ce91d2f194258f8acc347660b98e4c39ba
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-07-02 11:54:45 -0400

    Convergence in timestep

--- run/Test.jl
+++ run/Test.jl
@@ -3,7 +3,7 @@ push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
 ################ Librairies #############
 using ITensors
 using MBL
-
+using ProgressMeter
 ################ Parameters ###############
 
 N = 20
@@ -18,15 +18,30 @@ Dmax = 300
 Beta = n_sweep*δτ
 
 ################ Run basic ################
-mps, s = random_initialized_MPS(N, D)
-@show typeof(mps)
-update = tebdstepHeisenberg!(n_sweep, mps, h, δτ, cutoff, Dmax)
-@show update
-H = hamiltonianHeisenberg(update, h)
-@show typeof(H)
-e = measure_H(update, site_measure, H)
-@show e 
+function test()
+    mps, s = random_initialized_MPS(N, D)
+    @show typeof(mps)
+    update_test = tebdstepHeisenberg!(n_sweep, mps, h, δτ, cutoff, Dmax)
+    @show update_test
+    H_test = hamiltonianHeisenberg(update_test, h)
+    @show typeof(H_test)
+    e_test = measure_H(update_test, site_measure, H_test)
+    @show e_test 
+end
 ############## Run convergence #################
-
+EnergyList = Vector{}()
+mps_init, _ = random_initialized_MPS(N,D)
 time_list = reverse(collect(1e-4:1e-3:1.1e-2))
-@show time_list
+function void()
+    @showprogress for k in eachindex(time_list)
+        update = tebdstepHeisenberg!(n_sweep, mps_init, h, time_list[k], cutoff, Dmax)
+        H = hamiltonianHeisenberg(update, h)
+        e = measure_H(update, site_measure, H)
+        push!(EnergyList, e)
+    end
+end
+void()
+############ Plots #################
+gr()
+
+plot(time_list, EnergyList)
