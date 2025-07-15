ac03c873315649fc3174d07cc3e9ad5777d4d0ce
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-07-15 14:50:56 -0400

    energy heisenberg ancilla

--- run/Test.jl
+++ run/Test.jl
@@ -24,9 +24,9 @@ Dmax = 300
 betamax = 5
 step = 0.1
 Beta = n_sweep * δτ
-gammescale = 0.6
+gammescale = 0.8
 j = "z"
-γ=0.0
+γ = 0.0
 betalist = collect(0:step:betamax)
 
 function energysitetest(mps, sitemeasure, h)
@@ -34,18 +34,19 @@ function energysitetest(mps, sitemeasure, h)
     sn = siteind(copy, sitemeasure)
     snn = siteind(copy, sitemeasure + 1)
     gate =
-        op("Sz", sn) * op("Sz", snn) 
-    return gate
+        1 / 2 * op("S+", sn) * op("S-", snn) +
+        1 / 2 * op("S-", sn) * op("S+", snn) +
+        h * (op("Sz", sn) * op("Id", snn) + op("Id", sn) * op("Sz", snn))
     inter = copy[sitemeasure] * copy[sitemeasure+1]
     normalize!(inter)
-    @show gate
-    prout = replaceprime(gate, 0=>2)
+    #@show gate
+    prout = replaceprime(gate, 0 => 2)
     #@show replaceprime(gate, 0=>2)
     #@show dag(inter)
     #@show prout*inter
-    double = replaceprime(prout*inter, 2=>1)
+    double = replaceprime(prout * inter, 2 => 1)
     #@show scalar(double*dag(inter))
-    return real(scalar(double*dag(inter)))
+    return real(scalar(double * dag(inter)))
 end
 
 function energyagainstsitetest(mps, h, scale)
@@ -63,12 +64,12 @@ function energyagainstsitetest(mps, h, scale)
 end
 
 
-function energyforbetalist(betamax, step, ancilla, δτ, h, s, cutoff, op)
+function energyforbetalisttest(betamax, step, ancilla, δτ, h, s, cutoff, op)
     betalist = collect(0:step:betamax)
-    realbetalist= reverse(push!(diff(betalist), 0))
+    realbetalist = reverse(push!(diff(betalist), 0))
     Energylist = Vector{}(undef, length(realbetalist))
     update = ancilla
-    @showprogress desc="compute energy for β" for i in eachindex(realbetalist)
+    @showprogress desc = "compute energy for β" for i in eachindex(realbetalist)
         @info "β[$i]" betalist[i]
         update = MBL.TEBDancilla!(update, δτ, h, realbetalist[i], s, cutoff, op)
         _, Energylist[i] = energyagainstsitetest(update, h, gammescale)
@@ -81,15 +82,13 @@ test, s = MBL.AncillaMPO(N)
 #@show typeof(test)
 #@show test[3]
 #@show siteind(test, 3)
-@show energysitetest(test, 4, h)
+xdatasites, ydatadites = energyforbetalisttest(betamax, step, test, δτ, h, s, cutoff, "XY")
+xdataMPO, ydataMPO = MBL.energyforbetalist(betamax, step, test, δτ, h, s, cutoff, "XY")
+exacterngy1 = [MBL.exactenergyXY1(β, h, γ) for β in xdataMPO]
+exacterngy2 = [MBL.exactenergyXY2(beta) for beta in xdataMPO]
 
-@show energyagainstsitetest(test, h, gammescale)
-xdata, ydata = energyforbetalist(betamax, step, test, δτ, h, s, cutoff, "XY")
-exactdata = [MBL.exactenergyXY2(beta) for beta in xdata]
 gr()
-scatter(xdata, ydata, xlabel="β", ylabel="energie moyenne par site", title="N=$N, cutoff=$cutoff, δτ=$δτ, modèle XY", label="TEBD")
-#scatter!(xdata, -xdata/4)
-plot!(xdata, exactdata, label="exact energy")
-plot!(xdata[1:10], -xdata[1:10]/4, label="y=-x/4")
-#plot!(xdata[1:20], -J*J/4 .*xdata[1:20], label="y=-x/4")
-#hline!([1/4-log(2)], label="énergie exacte à température nulle")
+scatter(xdatasites, ydatadites, label="mesure par site", xlabel="β", ylabel="energy", title="model XY, N=$N, cutoff=$cutoff, δτ=$δτ")
+scatter!(xdataMPO, ydataMPO, label="mesure MPO")
+plot!(xdataMPO, exacterngy1, label="exact energy ChatGPT")
+plot!(xdataMPO, exacterngy2, label="S.Katsura exact energy")
