1f9800a2f318c80573f6752618a5f091055cf329
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-07-16 11:06:09 -0400

    organization

--- run/Test.jl
+++ run/Test.jl
@@ -36,10 +36,10 @@ input = JSON.parse(json_string)
 # ============================== DATA
 test, s = MBL.AncillaMPO(N)
 
-xdatasites, ydatadites = energyforbetalist(betamax, step, test, δτ, h, s, cutoff, "XY", gammescale)
-xdataMPO, ydataMPO = MBL.energyforbetalistMPO(betamax, step, test, δτ, h, s, cutoff, "XY")
+xdatasites, ydatadites = MBL.energyforbetalist(betamax, step, test, δτ, h, s, cutoff, "SS", gammescale)
+xdataMPO, ydataMPO = MBL.energyforbetalistMPO(betamax, step, test, δτ, h, s, cutoff, "SS")
 
-exactenergy = [MBL.exactenergyXY(β, h, γ) for β in xdataMPO]
+#exactenergy = [MBL.exactenergyXY(β, h, γ) for β in xdataMPO]
 
 #exactdz = [energyexact(input["spectrum"], beta, N) for beta in xdataMPO]
 
@@ -48,6 +48,6 @@ p = plot()
 scatter!(p, xdatasites, ydatadites, label="mesure par site")
 scatter!(p, xdataMPO, ydataMPO / N, label="mesure MPO")
 #plot!(xdataMPO, exactdz, label="exact dz", xlabel="β", ylabel="<H>/N", title="N=$N, δτ=$δτ, cutoff=$cutoff, model XY")
-plot!(p, xdataMPO, exactenergy, label=latexstring("-\\frac{1}{4π}\\int_{-π}^{π} cos(k)tanh(βcos(k)/2)dk"), xlabel="β", ylabel="<H>/N", title="N=$N, δτ=$δτ, cutoff=$cutoff, model XY")
+#plot!(p, xdataMPO, exactenergy, label=latexstring("-\\frac{1}{4π}\\int_{-π}^{π} cos(k)tanh(βcos(k)/2)dk"), xlabel="β", ylabel="<H>/N", title="N=$N, δτ=$δτ, cutoff=$cutoff, model XY")
 #hline!([1/4-log(2)], label="exact energy at zero temperature")
 display(p)
