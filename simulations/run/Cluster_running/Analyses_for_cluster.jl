f72b65e6685191e3ceb71ea990e15af55d2e3ceb
Roman Beauvallet <roman.beauvallet@ens-lyon.fr>
2025-09-01 13:50:29 +0200

    hello world

---
+++ simulations/run/Cluster_running/Analyses_for_cluster.jl
@@ -0,0 +1,29 @@
+#!usr/bin/env julia
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
+############### Libraries #################
+using MBL
+using ProgressMeter
+using JSON
+using Statistics
+using Dates
+using LinearAlgebra
+using Pkg
+# ===================== log
+println(Dates.format(Dates.now(), "yyyy-mm-dd HH:MM:SS"))
+println("Julia $VERSION")
+@show Base.julia_cmd()
+@show Threads.nthreads()
+@show LinearAlgebra.BLAS.get_num_threads()
+Pkg.status()
+# ===================== parameters
+if length(ARGS) < 1
+    println("Missing input file: use default")
+    throw(ErrorException)
+else
+    json_input = String(ARGS[1])
+end
+
+println("\nLoad input parameters from file $json_input")
+
+input_data = JSON.parsefile(json_input)
+map(k -> println(k, ": ", input_data[k]), sort(collect(keys(input_data))))
